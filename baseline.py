765eb3b57fad9c00541e7a6d6318bb5614564389
yaelshe <yael.shemesh9@gmail.com>
2024-05-21 02:24:57 +0300

    commit playground baseline changes

--- baseline.py
+++ baseline.py
@@ -5,15 +5,16 @@ import numpy as np
 import os
 from utils import Graph, fadeout_cur_fadein_next, get_partial_audio
 from utils import show_dtw_cost_matrix_and_wp  # for debug
-from spleeter.separator import Separator
+
+# from spleeter.separator import Separator
 
 
 os.environ['HF_HOME'] = '/home/joberant/NLP_2324/yaelshemesh'
 
-
 # songs_list_dir = '/home/yandex/APDL2324a/group_7/haviv_playlist/'
 # SONGS_LIST_DIR = '/home/joberant/NLP_2324/yaelshemesh/concert_songs/'
-SONGS_LIST_DIR = '/mnt/c/Users/ofirn/Music/songs/'
+# SONGS_LIST_DIR = '/mnt/c/Users/ofirn/Music/songs/'
+SONGS_LIST_DIR = 'playlist'
 NO_VOCAL_DETECTION_THRESHOLD = 5  # dB ; Threshold for silence detection in the vocals energy array.
 NOISE_THRESHOLD_FOR_TRANSFORMATION = 0  # dB ; Threshold for silence detection in transformation.
 MIN_SIZE_OF_TIME_INTERVAL = 2  # sec ; Minimum size of time interval for silence detection.
@@ -22,14 +23,15 @@ JUMP = 2  # Minimum number of frames between two silent intervals to consider th
 FADE_DURATION = 2.0  # sec ; Duration of the fade in and fade out effects.
 
 
-def connect_between_songs_by_dtw_only(song1: Song, song2: Song, file_path: str, song_builder: Song=None, accompaniment=False, pick_idx_method='min_dist_in_path'):
-    print(f"Concat {songs[i].song_name} and {songs[i+1].song_name} using DTW method (over chroma stft of the songs)")
+def connect_between_songs_by_dtw_only(song1: Song, song2: Song, file_path: str, song_builder: Song = None,
+                                      accompaniment=False, pick_idx_method='min_dist_in_path'):
+    print(f"Concat {songs[i].song_name} and {songs[i + 1].song_name} using DTW method (over chroma stft of the songs)")
     assert song1.sr == song2.sr, "sample rate should be the same in both songs"
-    
+
     # Get suffix of song1 and prefix of song2
     suffix_audio = song1.get_partial_audio(start_sec=-song1.partial_audio_time_in_sec)
     prefix_audio = song2.get_partial_audio(end_sec=song2.partial_audio_time_in_sec)
-    if accompaniment: # Use accompaniment audio only (if it has been separated)
+    if accompaniment:  # Use accompaniment audio only (if it has been separated)
         suffix_audio = song1.suffix_accompaniment.audio
         prefix_audio = song2.prefix_accompaniment.audio
 
@@ -39,8 +41,9 @@ def connect_between_songs_by_dtw_only(song1: Song, song2: Song, file_path: str, 
 
     # Use DTW to find the best match between suffix and prefix:
     sorted_indexes = []
-    window_in_sec = int(5 * song1.sr / 512) 
-    dtw_cost_matrix, wp = librosa.sequence.dtw(suffix_chroma_stft, prefix_chroma_stft, subseq=True)  # wp is the Warping path
+    window_in_sec = int(5 * song1.sr / 512)
+    dtw_cost_matrix, wp = librosa.sequence.dtw(suffix_chroma_stft, prefix_chroma_stft,
+                                               subseq=True)  # wp is the Warping path
     if pick_idx_method == 'random':
         sorted_indexes = np.random.choice(len(wp), len(wp))
     elif pick_idx_method == 'max_propagation':
@@ -49,7 +52,7 @@ def connect_between_songs_by_dtw_only(song1: Song, song2: Song, file_path: str, 
     elif pick_idx_method == 'min_dist_in_path':
         path_cost = dtw_cost_matrix[wp[:, 0], wp[:, 1]]
         sorted_indexes = np.argsort(path_cost[:len(path_cost) - window_in_sec] - path_cost[window_in_sec:])
-    
+
     # Among chosen indeces by the above methods, choose index in which suffix is not scilent:
     first_audio = None
     for idx in sorted_indexes:
@@ -60,7 +63,7 @@ def connect_between_songs_by_dtw_only(song1: Song, song2: Song, file_path: str, 
         suffix_cut_audio_index = len(first_song.audio) - (len(suffix_audio) - suffix_cut_frame * 512)
         first_audio = first_song.audio[: suffix_cut_audio_index]
         second_audio = song2.audio[prefix_cut_audio_index:]
-        if first_song.get_audio_energy_array(get_partial_audio(first_audio, first_song.sr, start_sec=-2))[0].mean()\
+        if first_song.get_audio_energy_array(get_partial_audio(first_audio, first_song.sr, start_sec=-2))[0].mean() \
                 > NOISE_THRESHOLD_FOR_TRANSFORMATION:
             best_tuple_in_sec = (suffix_cut_audio_index / first_song.sr, prefix_cut_audio_index / song2.sr)
             break
@@ -74,7 +77,8 @@ def connect_between_songs_by_dtw_only(song1: Song, song2: Song, file_path: str, 
 
 
 def find_silent_intervals_of_partial(song: Song, threshold=0, min_time_interval_length=2, suffix=False):
-    indices = np.where(song.suffix_vocals_energy < threshold)[0] if suffix else np.where(song.prefix_vocals_energy < threshold)[0]
+    indices = np.where(song.suffix_vocals_energy < threshold)[0] if suffix else \
+    np.where(song.prefix_vocals_energy < threshold)[0]
     scilent_time_intervals = []
     start = song.t_suffix[indices[0]] if suffix else song.t_prefix[indices[0]]
     for i in range(len(indices) - 1):
@@ -89,39 +93,47 @@ def find_silent_intervals_of_partial(song: Song, threshold=0, min_time_interval_
 def find_silent_intervals_of_suffix_and_prefix(song: Song, threshold=0, min_time_interval_length=2):
     suffix_scilent_intervals = find_silent_intervals_of_partial(song, threshold, min_time_interval_length, suffix=True)
     if len(suffix_scilent_intervals) == 0:
-        suffix_scilent_intervals = [(song.partial_audio_time_in_sec - MIN_SIZE_OF_TIME_INTERVAL, song.partial_audio_time_in_sec)]
+        suffix_scilent_intervals = [
+            (song.partial_audio_time_in_sec - MIN_SIZE_OF_TIME_INTERVAL, song.partial_audio_time_in_sec)]
     prefix_scilent_intervals = find_silent_intervals_of_partial(song, threshold, min_time_interval_length, suffix=False)
     if len(prefix_scilent_intervals) == 0:
         prefix_scilent_intervals = [(0, MIN_SIZE_OF_TIME_INTERVAL)]
     return prefix_scilent_intervals, suffix_scilent_intervals
 
 
-def dtw_over_songs_intervals_post_spleeter(song1: Song, song2: Song, song1_suffix_scilent_intervals, song2_prefix_scilent_intervals):
+def dtw_over_songs_intervals_post_spleeter(song1: Song, song2: Song, song1_suffix_scilent_intervals,
+                                           song2_prefix_scilent_intervals):
     min_value = np.inf
-    window_in_sec = int(1 * song1.sr / 512) # we take window of 1 sec 
+    window_in_sec = int(1 * song1.sr / 512)  # we take window of 1 sec
     min_idx = 0
     min_i = 0
     min_j = -1
     for i in range(len(song1_suffix_scilent_intervals)):
         for j in range(len(song2_prefix_scilent_intervals)):
-            chroma_suffix_song1 = song1.suffix_accompaniment.get_partial_chroma_stft(start_sec=song1_suffix_scilent_intervals[i][0],\
-                                                                end_sec=song1_suffix_scilent_intervals[i][1])
-            chroma_prefix_song2 = song2.prefix_accompaniment.get_partial_chroma_stft(start_sec=song2_prefix_scilent_intervals[j][0],\
-                                                                                    end_sec=song2_prefix_scilent_intervals[j][1])
-            dtw_cost_matrix, wp = librosa.sequence.dtw(chroma_suffix_song1, chroma_prefix_song2, subseq=True)  # wp is the Warping path
+            chroma_suffix_song1 = song1.suffix_accompaniment.get_partial_chroma_stft(
+                start_sec=song1_suffix_scilent_intervals[i][0], \
+                end_sec=song1_suffix_scilent_intervals[i][1])
+            chroma_prefix_song2 = song2.prefix_accompaniment.get_partial_chroma_stft(
+                start_sec=song2_prefix_scilent_intervals[j][0], \
+                end_sec=song2_prefix_scilent_intervals[j][1])
+            dtw_cost_matrix, wp = librosa.sequence.dtw(chroma_suffix_song1, chroma_prefix_song2,
+                                                       subseq=True)  # wp is the Warping path
             # Get the index of the minimum cost in the path
             wp_first_col = wp[:, 0] if max(wp[:, 0]) == dtw_cost_matrix.shape[0] - 1 else wp[:, 1]
             wp_second_col = wp[:, 1] if max(wp[:, 0]) == dtw_cost_matrix.shape[0] - 1 else wp[:, 0]
             path_cost = dtw_cost_matrix[wp_first_col, wp_second_col]
-            distances_in_path = path_cost[:len(path_cost)-window_in_sec] - path_cost[window_in_sec:]
+            distances_in_path = path_cost[:len(path_cost) - window_in_sec] - path_cost[window_in_sec:]
             sorted_indeces = np.argsort(distances_in_path)
             # Make sure that the suffix is not scilent:
             first_audio = None
             for idx in sorted_indeces:
                 suffix_cut_frame = wp[idx][0]
                 suffix_cut_audio_index = suffix_cut_frame * 512
-                first_audio = song1.get_partial_audio(end_sec=len(song1.audio)/song1.sr - song1.partial_audio_time_in_sec + song1_suffix_scilent_intervals[min_i][0] + suffix_cut_audio_index/song1.sr)
-                if song1.get_audio_energy_array(get_partial_audio(first_audio, song1.sr, start_sec=-2))[0].mean() > NOISE_THRESHOLD_FOR_TRANSFORMATION:
+                first_audio = song1.get_partial_audio(
+                    end_sec=len(song1.audio) / song1.sr - song1.partial_audio_time_in_sec +
+                            song1_suffix_scilent_intervals[min_i][0] + suffix_cut_audio_index / song1.sr)
+                if song1.get_audio_energy_array(get_partial_audio(first_audio, song1.sr, start_sec=-2))[
+                    0].mean() > NOISE_THRESHOLD_FOR_TRANSFORMATION:
                     break
             if distances_in_path[idx] < min_value:
                 min_value = distances_in_path[idx]
@@ -134,29 +146,31 @@ def dtw_over_songs_intervals_post_spleeter(song1: Song, song2: Song, song1_suffi
 def concat_between_songs_post_spleeter(song1: Song, song2: Song, file_path: str, song_builder: Song = None):
     print(f"Concat {song1.song_name} and {song2.song_name} using vocals and accompaniment separation then dtw")
     assert song1.sr == song2.sr, "sample rate should be the same in both songs"
-    
+
     # Get scilent time intervals of prefix and suffix of both songs:
-    song1_prefix_scilent_intervals, song1_suffix_scilent_intervals = find_silent_intervals_of_suffix_and_prefix(\
+    song1_prefix_scilent_intervals, song1_suffix_scilent_intervals = find_silent_intervals_of_suffix_and_prefix( \
         song1, threshold=NO_VOCAL_DETECTION_THRESHOLD, min_time_interval_length=MIN_SIZE_OF_TIME_INTERVAL)
-    song2_prefix_scilent_intervals, song2_suffix_scilent_intervals = find_silent_intervals_of_suffix_and_prefix(\
+    song2_prefix_scilent_intervals, song2_suffix_scilent_intervals = find_silent_intervals_of_suffix_and_prefix( \
         song2, threshold=NO_VOCAL_DETECTION_THRESHOLD, min_time_interval_length=MIN_SIZE_OF_TIME_INTERVAL)
-    
+
     print(f"{song1.song_name} suffix_scilent_intervals: ", song1_suffix_scilent_intervals)
     print(f"{song2.song_name} prefix_scilent_intervals: ", song2_prefix_scilent_intervals)
-    
+
     # Find the best interval to cut the audio in the right place and concat the two songs
     min_i, min_j, wp_min_idx = \
-        dtw_over_songs_intervals_post_spleeter\
+        dtw_over_songs_intervals_post_spleeter \
             (song1, song2, song1_suffix_scilent_intervals, song2_prefix_scilent_intervals)
-    
+
     # Cut the audio in the right place and concat the two songs
     suffix_cut_frame, prefix_cut_frame = wp_min_idx
     prefix_cut_audio_index = prefix_cut_frame * 512
     suffix_cut_audio_index = suffix_cut_frame * 512
     first_song = song_builder if song_builder is not None else song1
-    first = first_song.get_partial_audio(end_sec=len(first_song.audio)/song1.sr - song1.partial_audio_time_in_sec + song1_suffix_scilent_intervals[min_i][0] + suffix_cut_audio_index/song1.sr)
-    second = song2.get_partial_audio(prefix_cut_audio_index/song2.sr + song2_prefix_scilent_intervals[min_j][0])
-    #new_audio = fadeout_cur_fadein_next(first, second, song1.sr, duration=FADE_DURATION)
+    first = first_song.get_partial_audio(end_sec=len(first_song.audio) / song1.sr - song1.partial_audio_time_in_sec +
+                                                 song1_suffix_scilent_intervals[min_i][
+                                                     0] + suffix_cut_audio_index / song1.sr)
+    second = song2.get_partial_audio(prefix_cut_audio_index / song2.sr + song2_prefix_scilent_intervals[min_j][0])
+    # new_audio = fadeout_cur_fadein_next(first, second, song1.sr, duration=FADE_DURATION)
     new_audio = np.concatenate([first, second])
 
     sf.write(file_path, new_audio, first_song.sr)
@@ -166,23 +180,23 @@ def concat_between_songs_post_spleeter(song1: Song, song2: Song, file_path: str,
 
 def organize_song_list_using_dtw_greedy(songs):
     print("organizing songs using dtw greedy")
-    cur_song = songs[0] # assume first song is the first song in the playlist
+    cur_song = songs[0]  # assume first song is the first song in the playlist
     songs_set = set(songs)  # set of songs remain to be organized
-    songs_set.remove(cur_song) 
+    songs_set.remove(cur_song)
     organized_songs = []  # list of songs in the organized playlist
     organized_songs.append(cur_song)
-    
+
     while len(songs_set) > 0:
-        best_dtw_cost = np.inf # best dtw cost between two songs
-        best_song = None # best song to be added
+        best_dtw_cost = np.inf  # best dtw cost between two songs
+        best_song = None  # best song to be added
         for next_song in songs_set:
             # calculate dtw of cur song suffix with all other songs prefix
             suffix_audio = cur_song.get_partial_audio(start_sec=-cur_song.partial_audio_time_in_sec)
             prefix_audio = next_song.get_partial_audio(end_sec=next_song.partial_audio_time_in_sec)
-            suffix_chroma_stft = cur_song.get_chroma_stft(suffix_audio)  
-            prefix_chroma_stft = next_song.get_chroma_stft(prefix_audio) 
+            suffix_chroma_stft = cur_song.get_chroma_stft(suffix_audio)
+            prefix_chroma_stft = next_song.get_chroma_stft(prefix_audio)
             dtw_cost_matrix, wp = librosa.sequence.dtw(suffix_chroma_stft, prefix_chroma_stft, subseq=True)
-            dtw_cost = dtw_cost_matrix[-1,-1] 
+            dtw_cost = dtw_cost_matrix[-1, -1]
             if dtw_cost < best_dtw_cost:
                 best_dtw_cost = dtw_cost
                 best_song = next_song
@@ -198,26 +212,26 @@ def organize_song_list_using_dtw_optimum_aproximation(songs):
     n = len(songs)
     g = Graph(n)
     for i in range(n):
-        for j in range(i+1, n):
+        for j in range(i + 1, n):
             # calculate dtw of cur song suffix with all other songs prefix
             suffix_audio = songs[i].get_partial_audio(start_sec=-songs[i].partial_audio_time_in_sec)
             prefix_audio = songs[j].get_partial_audio(end_sec=songs[j].partial_audio_time_in_sec)
-            suffix_chroma_stft = songs[i].get_chroma_stft(suffix_audio)  
-            prefix_chroma_stft = songs[j].get_chroma_stft(prefix_audio) 
+            suffix_chroma_stft = songs[i].get_chroma_stft(suffix_audio)
+            prefix_chroma_stft = songs[j].get_chroma_stft(prefix_audio)
             dtw_cost_matrix, wp = librosa.sequence.dtw(suffix_chroma_stft, prefix_chroma_stft, subseq=True)
-            dtw_cost = dtw_cost_matrix[-1,-1] 
+            dtw_cost = dtw_cost_matrix[-1, -1]
             g.graph[i][j] = dtw_cost
             # calculate dtw of cur song prefix with all other songs suffix
             suffix_audio = songs[j].get_partial_audio(start_sec=-songs[j].partial_audio_time_in_sec)
             prefix_audio = songs[i].get_partial_audio(end_sec=songs[i].partial_audio_time_in_sec)
-            suffix_chroma_stft = songs[j].get_chroma_stft(suffix_audio)  
-            prefix_chroma_stft = songs[i].get_chroma_stft(prefix_audio) 
+            suffix_chroma_stft = songs[j].get_chroma_stft(suffix_audio)
+            prefix_chroma_stft = songs[i].get_chroma_stft(prefix_audio)
             dtw_cost_matrix, wp = librosa.sequence.dtw(suffix_chroma_stft, prefix_chroma_stft, subseq=True)
-            dtw_cost = dtw_cost_matrix[-1,-1] 
+            dtw_cost = dtw_cost_matrix[-1, -1]
             g.graph[j][i] = dtw_cost
     organized_songs_indexes = g.find_approximate_optimal_tsp_path()
     return [songs[idx] for idx in organized_songs_indexes]
-    
+
 
 def organize_song_list_using_tempo(songs):
     # Sort the songs by tempo
@@ -233,7 +247,7 @@ if __name__ == '__main__':
     sep = None
     for song_name in song_names:
         print(f"parsing: {song_name}")
-        song = Song(SONGS_LIST_DIR + f"{song_name}", seperator=sep, remove_zero_amp=True)
+        song = Song(os.path.join(SONGS_LIST_DIR, song_name), seperator=sep, remove_zero_amp=True)
         song.partial_audio_time_in_sec = 45
         time_in_sec = song.partial_audio_time_in_sec
         # song.calc_tempo()
@@ -242,7 +256,7 @@ if __name__ == '__main__':
         # song.find_vocals_and_accompaniment_for_suffix_and_prefix(dir_path=f"/home/joberant/NLP_2324/yaelshemesh/spleeter_output/{song_name}")
         # song.get_prefix_and_suffix_energy_array_post_separation()
         songs.append(song)
-        
+
         # song.suffix_vocals.plotter.plot_energy_and_rms(threshold=no_vocal_detection_threshold, plot_rms=True, title="suffix_vocal")
         # song.prefix_vocals.plotter.plot_energy_and_rms(threshold=no_vocal_detection_threshold, plot_rms=True, title="prefix_vocal")
 
@@ -250,16 +264,17 @@ if __name__ == '__main__':
 
     # songs = organize_song_list_using_tempo(songs)
     songs = organize_song_list_using_dtw_optimum_aproximation(songs)
-    #songs = organize_song_list_using_dtw_greedy(songs)
+    # songs = organize_song_list_using_dtw_greedy(songs)
     print([song.song_name for song in songs])
     new_song = songs[0]
     print('concatinating songs...')
-    outpath = "/mnt/c/Users/ofirn/Downloads/np/dtw_outputs"
+    outpath = "numpy_arrays\\dtw_outputs"
     result_fp = os.path.join(outpath, 'dtw_concat_playlist.wav')
     tuples_cuts_in_sec = []
-    for i in range(len(songs)-1):
-        #new_song = concat_between_songs_post_spleeter(songs[i], songs[i+1], file_path=result_fp, song_builder=new_song)
-        new_song, best_tuple_cuts_in_sec = connect_between_songs_by_dtw_only(songs[i], songs[i+1], file_path=result_fp, song_builder=new_song)
+    for i in range(len(songs) - 1):
+        # new_song = concat_between_songs_post_spleeter(songs[i], songs[i+1], file_path=result_fp, song_builder=new_song)
+        new_song, best_tuple_cuts_in_sec = connect_between_songs_by_dtw_only(songs[i], songs[i + 1],
+                                                                             file_path=result_fp, song_builder=new_song)
         tuples_cuts_in_sec.append(best_tuple_cuts_in_sec)
         # new_song = connect_between_songs_by_dtw_only(songs[i], songs[i+1], file_path=result_fp, song_builder=new_song, accompaniment=True)
     sf.write(result_fp, new_song.audio, new_song.sr)
@@ -268,4 +283,4 @@ if __name__ == '__main__':
     np.save(os.path.join(outpath, f'baseline_songs_name_order.npy'), np.array([song.song_name for song in songs]))
     np.save(os.path.join(outpath, f'best_cuts_in_sec.npy'), np.array(tuples_cuts_in_sec))
 
-    
+
